a424f98dab7970d8fdeecfdd1af0d766a056a030
hencydsouza <hencyhd39@gmail.com>
2026-08-12 00:29:47 +0530

    chore: astryx setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
+import { Theme } from "@astryxdesign/core/theme";
+import { neutralTheme } from "@astryxdesign/theme-neutral/built";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@astryxdesign/core/reset.css";
+import "@astryxdesign/core/astryx.css";
+import "@astryxdesign/theme-neutral/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Theme theme={neutralTheme}>
+      <App />
+    </Theme>
   </React.StrictMode>
 );
